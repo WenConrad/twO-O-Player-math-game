e64dbf64a61101b9ff11a88ed2ef22e6eed11bff
WenConrad <conradwen@gmail.com>
2022-01-05 20:13:58 -0800

    bug fix

--- main.rb
+++ main.rb
@@ -7,15 +7,16 @@ puts "Player 1 Name:"
 puts "Player 2 Name:"
 @p2 = Player.new(gets.chomp)
 current_turn = @p1
-while @p1.score && @p2.score > 0
-  puts "----- NEW TURN -----"
+while true
   if !Question.new(current_turn).prompt
     current_turn.incorrect
   end
-  puts "#{@p1.player_score} vs #{@p2.player_score}"
+  puts "->#{@p1.player_score} vs #{@p2.player_score}<-"
+  if @p1.score == 0 || @p2.score == 0
+    break
+  end
   current_turn = current_turn == @p1 ? @p2 : @p1
+  puts "----- NEW TURN -----"
 end
 winner = @p1.score > @p2.score ? @p1 : @p2
-puts "#{winner} wins with a score of #{winner.score}/3"
-puts "----- GAME OVER -----"
-puts "Good bye!"
+puts "#{winner} wins with a score of #{winner.score}/3\n----- GAME OVER -----\nGood bye!"
