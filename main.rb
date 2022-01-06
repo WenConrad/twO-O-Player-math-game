672670f01a723549e02632a4012b65857d7799a4
WenConrad <conradwen@gmail.com>
2022-01-05 19:15:40 -0800

    get player score

--- main.rb
+++ main.rb
@@ -3,3 +3,5 @@ require './player'
 p1 = Player.new('Conrad')
 
 puts p1.inspect
+
+puts p1.player_score
--- player.rb
+++ player.rb
@@ -1,7 +1,17 @@
 class Player
   attr_accessor :score
+  attr_reader :name
+
   def initialize(name)
     @name = name
     @score = 3
+  end
+
+  def incorrect
+    self.score -= 1
+  end
+
+  def player_score
+    "#{self.name}: #{self.score}/3"
   end
 end
