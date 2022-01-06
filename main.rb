554f3a57893fe1cf0cb2949da5900f671d23fa2e
WenConrad <conradwen@gmail.com>
2022-01-05 20:03:53 -0800

    working game

--- main.rb
+++ main.rb
@@ -4,9 +4,18 @@ require './question'
 puts "Welcome to the TwO-O-Player Math Game!"
 puts "Player 1 Name:"
 @p1 = Player.new(gets.chomp)
-puts @p1.inspect
-if !Question.new(@p1).prompt
-  @p1.incorrect
+puts "Player 2 Name:"
+@p2 = Player.new(gets.chomp)
+current_turn = @p1
+while @p1.score && @p2.score > 0
+  puts "----- NEW TURN -----"
+  if !Question.new(current_turn).prompt
+    current_turn.incorrect
+  end
+  puts "#{@p1.player_score} vs #{@p2.player_score}"
+  current_turn = current_turn == @p1 ? @p2 : @p1
 end
-
-puts @p1.inspect
+winner = @p1.score > @p2.score ? @p1 : @p2
+puts "#{winner} wins with a score of #{winner.score}/3"
+puts "----- GAME OVER -----"
+puts "Good bye!"
